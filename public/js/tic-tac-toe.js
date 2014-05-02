d25e70cb4407faa9f24797597c385b3d9509aa10
Nader Chehab <naderchehab@gmail.com>
2014-05-02 12:38:49 -0400

    Added random player

--- public/js/tic-tac-toe.js
+++ public/js/tic-tac-toe.js
@@ -6,15 +6,16 @@ $(function () {
             oMarks: 0,
             xMarks: 0,
             boardSize: 3,
+            gameEnded: false,
             currentPlayer: "x",
-            wins: [
-                 "111" +
-                 "000" +
-                 "000",
+            winnerPatterns: [
+                "111" +
+                "000" +
+                "000",
 
-                 "000" +
-                 "111" +
-                 "000",
+                "000" +
+                "111" +
+                "000",
 
                 "000" +
                 "000" +
@@ -43,13 +44,13 @@ $(function () {
     });
 
     var BoardView = Backbone.View.extend({
-        el: $("table"),
+        el: $("table.board"),
         initialize: function () {
-            _.bindAll(this, "render", "markCell");
+            _.bindAll(this, "render", "markCell", "checkWin", "getLegalMoves", "getComputerMove");
             this.render();
         },
         events: {
-          "click td": "markCell"
+            "click td": "markCell"
         },
         render: function () {
             var boardSize = this.model.get("boardSize");
@@ -59,31 +60,69 @@ $(function () {
             }
 
             for (var i = 0; i < boardSize; i++) {
-                $("tr").append("<td />");
+                $(this.el).find("tr").append("<td />");
             }
 
-            $.each($("td"), function (i, e) {
+            $.each($(this.el).find("td"), function (i, e) {
                 $(e).attr("id", "cell" + Math.pow(2, i));
             });
 
             return this; // for chainable calls, like .render().el
         },
-        markCell: function(e) {
+        markCell: function (e) {
+            var model = this.model;
             var element = $(e.currentTarget);
             var id = element.attr("id");
             var cellNumber = id.replace("cell", "");
-            var xMarks = this.model.get("xMarks");
-            var oMarks = this.model.get("oMarks");
-            var currentPlayer = this.model.get("currentPlayer");
+            var currentPlayer = model.get("currentPlayer");
+            var playerMarks = currentPlayer + "Marks";
 
-            if ((cellNumber & xMarks) == cellNumber || (cellNumber & oMarks) == cellNumber) {
-                console.log("Already set!");
-            }
-            else {
-                this.model.set(currentPlayer + "Marks",  this.model.get(currentPlayer + "Marks") | cellNumber);
+            if (!model.get("gameEnded") && cellNumber != (cellNumber & model.get("xMarks")) && cellNumber != (cellNumber & model.get("oMarks"))) {
+                model.set(playerMarks, model.get(playerMarks) | cellNumber);
                 element.append(currentPlayer.toUpperCase());
-                this.model.set("currentPlayer", currentPlayer == "x" ? "o" : "x");
+
+                if (this.checkWin(currentPlayer)) {
+                    return;
+                }
+
+                model.set("currentPlayer", "o");
+                currentPlayer = model.get("currentPlayer");
+                playerMarks = currentPlayer + "Marks";
+                var cellNumber = this.getComputerMove(model.get("xMarks"), model.get("oMarks"));
+                model.set(playerMarks, model.get(playerMarks) | cellNumber);
+                $(this.el).find("#cell" + cellNumber).append(currentPlayer.toUpperCase());
+                this.checkWin(currentPlayer);
+                model.set("currentPlayer", "x");
             }
+        },
+        checkWin: function(currentPlayer) {
+            var model = this.model;
+            $.each(model.get("winnerPatterns"), function (index, value) {
+                var winnerPattern = parseInt(value, 2);
+                if ((winnerPattern & model.get("xMarks")) == winnerPattern || (winnerPattern & model.get("oMarks")) == winnerPattern) {
+                    alert(currentPlayer + " wins!");
+                    model.set("gameEnded", true);
+                }
+            });
+            return model.get("gameEnded");
+        },
+        getComputerMove: function (xMarks, oMarks) {
+            var legalMoves = this.getLegalMoves(xMarks, oMarks);
+            var rand = Math.floor(Math.random() * (legalMoves.length + 1));
+            return legalMoves[rand];
+        },
+        getLegalMoves: function (xMarks, oMarks) {
+            var model = this.model;
+            var validCells = [];
+            var numCells = model.get("boardSize") * model.get("boardSize");
+            for (var i = 0; i < numCells; i++) {
+                var cellNumber = Math.pow(2, i);
+
+                if (cellNumber != (cellNumber & xMarks) && cellNumber != (cellNumber & oMarks)) {
+                    validCells.push(cellNumber);
+                }
+            }
+            return validCells;
         }
     });
 
